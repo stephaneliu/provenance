663a6c161464a1f1570f7da1490f70d2ee9d1acc
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-18 15:45:14 -1000

    Chore: Config Tailwind Stimulus Components

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,15 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// Import and register all TailwindCSS Components or just the ones you need
+import { Alert, Autosave, ColorPreview, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('color-preview', ColorPreview)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('popover', Popover)
+application.register('slideover', Slideover)
+application.register('tabs', Tabs)
+application.register('toggle', Toggle)
